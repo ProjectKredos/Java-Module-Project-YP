c9719ea8411b8377866d2a6bcd58948d6718d46a
ProjectKredos <115523501+ProjectKredos@users.noreply.github.com>
2023-01-02 12:36:09 +0500

    Исправлены: 1) Корректность склонения "Рубли"; 2) Использован String Bilder; 3) Добавлены проверки: - Ввод текста вместо цифр - Ввод отрицательных цен 4) Выровнен код через Ctrl + Alt + Shift + L 5) Исправлено написание классов "С большой буквы"

--- src/main/java/Goods.java
+++ src/main/java/Goods.java
@@ -31,14 +31,12 @@ public class Goods {
                     System.out.println("Цена может быть только числом ХХ,ХХ");
                 }
             }
-            //проверка что введена цифра hasNextInt()
             System.out.println("Товар " + goodsName + " за " + goodsPrice + " " + rubToText(goodsPrice) + " успешно добавлен. Добавьте еще или завершите.");
             goodsList.append("\n").append(goodsName).append(" ").append(goodsPrice);
             goodsPriceSum = goodsPriceSum + goodsPrice;
         }
         System.out.println(goodsList + "\n" + "На общую сумму: " + String.format("%.2f", goodsPriceSum) + " " + rubToText(goodsPriceSum));
         System.out.println("С каждого из " + countUsers + " по " + String.format("%.2f", goodsPriceSum / countUsers) + " " + rubToText(goodsPriceSum / countUsers));
-        //String.format("%.2f", double);
     }
 
     public static String rubToText(double sumRub) {
