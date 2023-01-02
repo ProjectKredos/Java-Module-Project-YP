347e31a4164e396d0ba134889a5b36cf2e2ca539
ProjectKredos <115523501+ProjectKredos@users.noreply.github.com>
2023-01-02 12:28:31 +0500

    Исправлены: 1) Корректность склонения "Рубли"; 2) Использован String Bilder; 3) Добавлены проверки: - Ввод текста вместо цифр - Ввод отрицательных цен 4) Выровнен код через Ctrl + Alt + Shift + L 5) Исправлено написание классов "С большой буквы"

---
+++ src/main/java/Goods.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Goods {
+
+    static String goodsName;
+    static double goodsPrice;
+    static double goodsPriceSum;
+
+
+    public static void addGoods(int countUsers) {
+        StringBuilder goodsList = new StringBuilder();
+        goodsList.append("Добавленные товары:");
+        System.out.println("Введите название товара.");
+        Scanner goodsScanner = new Scanner(System.in);
+        while (true) {
+            goodsName = goodsScanner.nextLine();
+            if (goodsName.trim().equalsIgnoreCase("Завершить")) {
+                break;
+            }
+            System.out.println("Введите цену товара");
+            while (true) {
+                Scanner priceScanner = new Scanner(System.in);
+                if (priceScanner.hasNextDouble()) {
+                    goodsPrice = priceScanner.nextDouble();
+                    if (goodsPrice < 0) {
+                        goodsPrice = Math.abs(goodsPrice);
+                        System.out.println("Цена преорбразована в положительное значение " + goodsPrice);
+                    }
+                    break;
+                } else {
+                    System.out.println("Цена может быть только числом ХХ,ХХ");
+                }
+            }
+            //проверка что введена цифра hasNextInt()
+            System.out.println("Товар " + goodsName + " за " + goodsPrice + " " + rubToText(goodsPrice) + " успешно добавлен. Добавьте еще или завершите.");
+            goodsList.append("\n").append(goodsName).append(" ").append(goodsPrice);
+            goodsPriceSum = goodsPriceSum + goodsPrice;
+        }
+        System.out.println(goodsList + "\n" + "На общую сумму: " + String.format("%.2f", goodsPriceSum) + " " + rubToText(goodsPriceSum));
+        System.out.println("С каждого из " + countUsers + " по " + String.format("%.2f", goodsPriceSum / countUsers) + " " + rubToText(goodsPriceSum / countUsers));
+        //String.format("%.2f", double);
+    }
+
+    public static String rubToText(double sumRub) {
+        int intRub = ((int) sumRub);
+        String Rubs = "рублей";
+        if (intRub % 10 == 1) {
+            Rubs = "рубль";
+        }
+        if ((intRub % 10 >= 2) && (intRub % 10 <= 4)) {
+            Rubs = "рубля";
+        }
+        if ((intRub % 100 >= 11) && (intRub % 100 <= 20)) {
+            Rubs = "рублей";
+        }
+        return (Rubs);
+    }
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,19 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         int countUser;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("На скольких человек необходимо разделить счёт?");
-        while (true) {
-            countUser = scanner.nextInt();
-            if (countUser==1) {
-                System.out.println("В этом случае нет смысла ничего считать и делить. Попробуйте еще раз.");
-            } else if (countUser<1) {
-                System.out.println("Это некорректное значение для подсчёта. Попробуйте еще раз.");
-            } else {
-                break;
-            }
-        }
-        goods.addGoods(countUser);
+        countUser = countUser();
+        Goods.addGoods(countUser);
 
     }
+
+    public static int countUser() {
+        int countUser;
+        System.out.println("На скольких человек необходимо разделить счёт?");
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            if (scanner.hasNextInt()) {
+                countUser = scanner.nextInt();
+                if (countUser == 1) {
+                    System.out.println("В этом случае нет смысла ничего считать и делить. Попробуйте еще раз.");
+                } else if (countUser < 1) {
+                    System.out.println("Это некорректное значение для подсчёта. Попробуйте еще раз.");
+                } else {
+                    return (countUser);
+                }
+            } else {
+                System.out.println("Это не число");
+            }
+        }
+
+    }
+
 }
